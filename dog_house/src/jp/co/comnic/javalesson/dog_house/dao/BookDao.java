59e91e15b695fdc75d4e7c955bd361cd07a93810
igawa.yoshiki <igawa.yoshiki@igawa>
2017-06-22 11:07:19 +0900

    2017/06/22　検索機能実装(中)

--- dog_house/src/jp/co/comnic/javalesson/dog_house/dao/BookDao.java
+++ dog_house/src/jp/co/comnic/javalesson/dog_house/dao/BookDao.java
@@ -1,13 +1,40 @@
 package jp.co.comnic.javalesson.dog_house.dao;
 
+
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+
 import jp.co.comnic.javalesson.dog_house.entity.Book;
 
-public class BookDao extends BaseDao {
+public class BookDao extends BaseDao  {
+	
+	CriteriaQuery<Book> queryForBook =  builder.createQuery(Book.class);
+	Root<Book> bookroot = queryForBook.from(Book.class);
+	Integer categoryId;
+	private Book book;
 	
 	
 	public Book  findById(Integer id){
 		return super.findById(Book.class, id);
-		
 	} 
+	
+	public  Book findByCategoryId(Integer id){
+		
+		queryForBook.select(bookroot).where(categoryId = id);
+		
+		
+		book = em.createQuery(queryForBook.select(bookroot))
+				.getSingleResult();
+
+		
+	
+				
+		
+		return book;
+		
+		
+		
+	}
 	
 }
